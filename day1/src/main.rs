e0a652ce803b76f332f3d88383fb2089f3a00c04
Mikołaj Walczak <mikiwalczak+github@gmail.com>
2022-12-03 18:41:09 +0000

    Day 1 - part 2

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -2,7 +2,8 @@ use std::cmp;
 use std::io;
 use std::io::BufRead;
 
-fn main() -> io::Result<()> {
+#[allow(dead_code)]
+fn part_one() {
     let stdin = io::stdin();
     let mut lines = stdin.lock().lines();
 
@@ -11,13 +12,38 @@ fn main() -> io::Result<()> {
 
     while let Some(Ok(line)) = lines.next() {
         if "".eq(&line) {
-            current_calories = 0 
+            current_calories = 0;
         } else {
             let calories: i32 = line.parse().unwrap();
             current_calories += calories;
-            max_calories = cmp::max(current_calories, max_calories)
+            max_calories = cmp::max(current_calories, max_calories);
         }
     }
     println!("{}", max_calories);
+}
+
+fn part_two() {
+    let stdin = io::stdin();
+    let mut lines = stdin.lock().lines();
+
+    let mut max_calories: [i32; 3] = [0; 3];
+    let mut current_calories: i32 = 0;
+
+    while let Some(Ok(line)) = lines.next() {
+        if "".eq(&line) {
+            max_calories[0] = cmp::max(current_calories, max_calories[0]);
+            max_calories.sort();
+            current_calories = 0;
+        } else {
+            let calories: i32 = line.parse().unwrap();
+            current_calories += calories;
+        }
+    }
+    println!("{}", max_calories.iter().sum::<i32>());
+}
+
+
+fn main() -> io::Result<()> {
+    part_two();
     Ok(())
 }
