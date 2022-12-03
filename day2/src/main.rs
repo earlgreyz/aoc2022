11e1ea3b5a984f5018652fddbd4adb496be9bee1
Mikołaj Walczak <mikiwalczak+github@gmail.com>
2022-12-03 18:57:07 +0000

    Day 2 - part 2

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,6 +1,7 @@
 use std::io;
 use std::io::BufRead;
 
+#[allow(dead_code)]
 fn part_one() {
     let stdin = io::stdin();
     let mut lines = stdin.lock().lines();
@@ -25,6 +26,31 @@ fn part_one() {
     println!("{}", score);
 }
 
+fn part_two() {
+    let stdin = io::stdin();
+    let mut lines = stdin.lock().lines();
+
+    let mut score: i32 = 0;
+
+    while let Some(Ok(line)) = lines.next() {
+        let result = match line.trim().as_ref() {
+            "A X" => 3 + 0,
+            "A Y" => 1 + 3,
+            "A Z" => 2 + 6,
+            "B X" => 1 + 0,
+            "B Y" => 2 + 3,
+            "B Z" => 3 + 6,
+            "C X" => 2 + 0,
+            "C Y" => 3 + 3,
+            "C Z" => 1 + 6,
+            _ => 0,
+        };
+        score += result
+    }
+    println!("{}", score);
+}
+
+
 fn main() {
-    part_one()
+    part_two()
 }
