21fc5c775f4367c9e1444cff5ecf0c423181d875
Mikołaj Walczak <mikiwalczak+github@gmail.com>
2022-12-12 15:51:27 +0000

    Day 12 - part 2

--- day12/src/main.rs
+++ day12/src/main.rs
@@ -58,6 +58,7 @@ fn add_neighbours(position: Position, step: usize, map: &Map, queue: &mut VecDeq
     }
 }
 
+#[allow(dead_code)]
 fn part_one(map: &Vec<Vec<i32>>, start: Position, end: Position) {
     let mut queue = VecDeque::new();
     let mut visited = Vec::new();
@@ -84,7 +85,53 @@ fn part_one(map: &Vec<Vec<i32>>, start: Position, end: Position) {
     }
 }
 
+fn add_neighbours_from(position: Position, step: usize, map: &Map, queue: &mut VecDeque<(usize, Position)>) {
+    let (x, y) = position;
+    // Move from left.
+    if x > 0 && can_move((x - 1, y), position, map) {
+        queue.push_back((step, (x - 1, y)));
+    }
+    // Move from right.
+    if x < map[y].len() - 1 && can_move((x + 1, y), position, map) {
+        queue.push_back((step, (x + 1, y)));
+    }
+    // Move from up.
+    if y > 0 && can_move((x, y - 1), position, map) {
+        queue.push_back((step, (x, y - 1)));
+    }
+    // Move from down.
+    if y < map.len() - 1 && can_move((x, y + 1), position, map) {
+        queue.push_back((step, (x, y + 1)));
+    }
+}
+
+fn part_two(map: &Vec<Vec<i32>>, end: Position) {
+    let mut queue = VecDeque::new();
+    let mut visited = Vec::new();
+    for y in 0..map.len() {
+        let mut row = Vec::new();
+        for _ in 0..map[y].len() {
+            row.push(false);
+        }
+        visited.push(row);
+    }
+
+    add_neighbours_from(end, 1, map, &mut queue);
+    visited[end.1][end.0] = true;
+
+    while let Some((step, position)) = queue.pop_front() {
+        if map[position.1][position.0] == 0 {
+            println!("{}", step);
+            break;
+        }
+        if !visited[position.1][position.0] {
+            add_neighbours_from(position, step + 1, map, &mut queue);
+            visited[position.1][position.0] = true;
+        }
+    }
+}
+
 fn main() {
-    let (map, start, end) = read_map();
-    part_one(&map, start, end);
+    let (map, _start, end) = read_map();
+    part_two(&map, end);
 }
