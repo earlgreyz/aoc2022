060b6ecb5a8e1aca410802d7c46af2e5aa0cd713
Mikołaj Walczak <mikiwalczak+github@gmail.com>
2022-12-15 17:42:38 +0000

    Refactor day 15 part 1

--- day15/src/main.rs
+++ day15/src/main.rs
@@ -32,35 +32,60 @@ impl Sub for Point {
     }
 }
 
-fn main() {
+struct Beacon {
+    position: Point,
+}
+
+struct Sensor {
+    position: Point,
+    radius: i32
+}
+
+fn read_sensors() -> (Vec<Sensor>, Vec<Beacon>) {
     let stdin = io::stdin();
-    let points: Vec<(Point, Point)> = stdin.lock().lines().map(|line| line.unwrap()).map(|line| {
+    let mut lines = stdin.lock().lines();
+
+    let mut beacons_uniq = HashSet::new();
+    let mut sensors = Vec::new();
+
+    while let Some(Ok(line)) = lines.next() {
         let captures = RE.captures(line.as_ref()).unwrap();
+
         // Sensor
         let sx = captures.name("sx").unwrap().as_str().parse().unwrap();
         let sy = captures.name("sy").unwrap().as_str().parse().unwrap();
+        let sensor_position = Point{ x: sx, y: sy };
+
         // Beacon
         let bx = captures.name("bx").unwrap().as_str().parse().unwrap();
         let by = captures.name("by").unwrap().as_str().parse().unwrap();
-        (Point{ x: sx, y: sy }, Point{ x: bx, y: by })
-    }).collect();
+        let beacon_position = Point{ x: bx, y: by };
 
-    let beacons_at_row: HashSet<(i32, i32)> = points.iter().map(|(_, beacon)| (beacon.x, beacon.y)).filter(|(_, y)| *y == SEARCH_ROW).collect();
+        beacons_uniq.insert((bx, by));
+        sensors.push(Sensor{ position: sensor_position, radius: (sensor_position - beacon_position).length() });
+    }
 
-    let mut tree = IntervalTree::new();
-    for (sensor, beacon) in points {
-        let distance = sensor - beacon;
-        let length = distance.length();
+    let beacons: Vec<Beacon> = beacons_uniq.iter().map(|(x, y)| Beacon{ position: Point{ x: *x, y: *y }}).collect();
+    (sensors, beacons)
+}
 
-        let vertical = (sensor.y - SEARCH_ROW).abs();
-        let horizontal = length - vertical;
+fn part_one() {
+    let (sensors, beacons) = read_sensors();
+    let mut row = IntervalTree::new();
 
+    for sensor in sensors.iter() {
+        let vertical = (sensor.position.y - SEARCH_ROW).abs();
+        let horizontal = sensor.radius - vertical;
         if horizontal >= 0 {
-            let interval = (sensor.x - horizontal, sensor.x + horizontal);
-            tree.insert(interval);
+            let interval = (sensor.position.x - horizontal, sensor.position.x + horizontal);
+            row.insert(interval);
         }
     }
 
+    let overlapping_beacons = beacons.iter().filter(|beacon| beacon.position.y == SEARCH_ROW).count();
+    println!("{}", row.size() - overlapping_beacons as i32);
+}
 
-    println!("{}", tree.size() - beacons_at_row.len() as i32);
+fn main() {
+    part_one();
 }
